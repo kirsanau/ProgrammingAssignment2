c699c51e293a76c6cf0d12fbf3097279e6c6c36c
Ilia Kirsanau <ilia.kirsanau@altasapiens.com>
2015-01-18 22:53:50 +0300

    Implemented invertion functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These 2 functions able to create an object that keeps the matrix
+## and it's invert matrix, to retrive latter one from cache
 
-## Write a short comment describing this function
+## Function makeCacheMatrix takes a matrix and creates an object 
+## with 4 get/set methohds to return original and inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv_x <- NULL
+    set <- function(y) {
+        x <<- y
+        inv_x <<- NULL
+    }
+    get <- function() x
+    set_inv <- function(inv) inv_x <<- inv
+    get_inv <- function() inv_x
+    list(set = set, get = get,
+         set_inv = set_inv,
+         get_inv = get_inv)
 }
 
 
-## Write a short comment describing this function
+## creates inverted matrix, if inverted exists - takes it from cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    
+    inv_x <- x$get_inv()
+    if(!is.null(inv_x)){
+        message("getting cached data")
+        return(inv_x)
+    }
+    data <- x$get()
+    inv_x <- solve(data, ...)
+    x$set_inv(inv_x)
+    inv_x
 }
